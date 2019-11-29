2d68139d7dfe595f01f13b66eb0959a486faff1e
AlyzaG <alyza.gumma-saad@devinci.fr>
2019-11-29 16:17:56 +0100

    add search bar on js to get data

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ document.addEventListener("click", function () {
     $('.sidenav').removeClass('over');
     $('.sidenav2').removeClass('over2');
 });
-
-
-
 /*APPel À L'API*/
 
 function getApiData() {
@@ -46,6 +43,7 @@ function getApiData() {
         }
 
     }
+
 
     xhr.open('GET', 'https://gateway.marvel.com/v1/public/characters?name=spider-man&apikey=b7287f791e9cd24e6791c69c4eafbf76')
     xhr.send()
